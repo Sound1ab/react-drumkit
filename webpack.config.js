59ed91dbf0d62bfe36f684516ec9b3de9c977a91
Phillip Parker <phillip.parker@yourfavouritestory.com>
2016-12-12 12:41:18 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const config = {
+    entry: ['webpack/hot/dev-server' , APP_DIR + '/index.jsx'],
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+        publicPath: '/public/'
+    },
+    module : {
+        preLoaders: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                loaders: ['eslint'],
+                include: APP_DIR
+
+            }
+        ],
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
+        ]
+    }
+};
+
+module.exports = config;
